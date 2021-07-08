1a42e9c1c83d08b708593af724bb526caac212f9
appletime81 <itrimsl8696@gmail.com>
2021-07-09 00:13:49 +0800

    Update

--- main.py
+++ main.py
@@ -51,33 +51,46 @@ def main():
             data_dict_1["Machine_name"].append(machine_name)
             data_dict_1["Status"].append(status_name)
             data_dict_1["DTTM"].append(log_datetime)
-            data_dict_1["Even_type"].append(is_nan(event_type))
+            data_dict_1["Even_type"].append("")
             data_dict_1["Remark"].append(is_nan(error_name))
-        elif i > 0:
-            if event_type == "STATUS" and status_name == "FINISHED" and stopped_reason == "Auto":
+        elif i > first_started_index:
+            if event_type == "STATUS" and status_name == "STARTED" and isnan(stopped_reason):
                 data_dict_1["Machine_name"].append(machine_name)
-                data_dict_1["Status"].append(status_name)
+                data_dict_1["Status"].append("STARTED")
                 data_dict_1["DTTM"].append(log_datetime)
-                data_dict_1["Even_type"].append(is_nan(event_type))
-                data_dict_1["Remark"].append(is_nan(error_name))
-            elif event_type == "ERROR" and error_code.replace(".", "").isdigit():
+                data_dict_1["Even_type"].append("")
+                data_dict_1["Remark"].append("")
+            elif event_type == "STATUS" and status_name == "FINISHED" and stopped_reason == "Auto":
+                pass
+            elif event_type == "STATUS" and status_name == "FINISHED" and stopped_reason == "MA":
+                data_dict_1["Machine_name"].append(machine_name)
+                data_dict_1["Status"].append("FINISHED")
+                data_dict_1["DTTM"].append(log_datetime)
+                data_dict_1["Even_type"].append(stopped_reason)
+                data_dict_1["Remark"].append("")
+            elif event_type == "ERROR" and str(error_code).replace(".", "").isdigit():
                 data_dict_1["Machine_name"].append(machine_name)
                 data_dict_1["Status"].append("FINISHED")
                 data_dict_1["DTTM"].append(log_datetime)
                 data_dict_1["Even_type"].append(is_nan(error_code))
                 data_dict_1["Remark"].append(is_nan(error_name))
-            elif event_type == "STATUS" and status_name == "FINISHED" and raw_df["event_type"][i-1] == "ERROR":
-                pass  # doing nothing
             elif event_type == "ALARM" and status_name == "Begin Repair":
                 data_dict_1["Machine_name"].append(machine_name)
                 data_dict_1["Status"].append("ALARM")
                 data_dict_1["DTTM"].append(log_datetime)
+                data_dict_1["Even_type"].append(is_nan(status_name))
+                data_dict_1["Remark"].append(is_nan(error_name))
+            elif event_type == "MACHINE" and status_name == "State: Specific Running Continuous":
+                data_dict_1["Machine_name"].append(machine_name)
+                data_dict_1["Status"].append("MACHINE")
+                data_dict_1["DTTM"].append(log_datetime)
                 data_dict_1["Even_type"].append(is_nan(error_code))
                 data_dict_1["Remark"].append(is_nan(error_name))
-            # TODO: event_type為MACHINE的判斷式
-            # elif
 
-    # pprint(data_dict_1)
+    result_df = pd.DataFrame(data_dict_1)
+
+    # save result to the file
+    result_df.to_csv("test.csv", index=False)
 
 
 if __name__ == "__main__":
