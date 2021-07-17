1a581d8a67d076fa865fb18a94d936028680cd47
appletime81 <itrimsl8696@gmail.com>
2021-07-17 23:45:20 +0800

    Update

---
+++ main2.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from datetime import datetime
+from math import isnan
+from pprint import pprint
+
+
+def get_machine_name(raw_data_file):
+    df = pd.read_csv(raw_data_file)
+    machine_names = list(df.groupby(["machine_name"]).groups.keys())
+    return machine_names
+
+
+def policy(df):
+    # variables
+    col_names = df.columns
+    res = {
+        "Machine": [], "Status": [], "Start_DTTM": [], "Stop_DTTM": [], "Duration": [],
+        "error_code": [], "error_name": [], "stopped_reason": []
+    }
+
+    for i in range(len(df)):
+        serial, log_datetime, machine_name, event_type, status_name, error_code, error_name, stopped_reason = \
+            [df[key][i] for key in col_names]
+        # print(serial, log_datetime, machine_name, event_type, status_name, error_code, error_name, stopped_reason)
+
+        # start processing
+        if i > 0:
+            pass
+    
+
+
+if __name__ == "__main__":
+    # get_machine_name("raw_data.csv")
+    df = pd.read_csv("raw_data.csv")
+    policy(df)
--- tes.py
+++
@@ -1,5 +0,0 @@
-a = "2"
-print(a.replace(".", "").isdigit())
-
-b = "2.0"
-print(b.replace('.', '').isdigit())
